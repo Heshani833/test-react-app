86a73eb9c7d1d9e9203f37372dff0cd5fa09623d
Heshani833 <heshanishehana123@gmail.com>
2025-07-28 13:40:47 +0530

    fix content

--- test-react-app/src/userForm.js
+++ test-react-app/src/userForm.js
@@ -1,61 +1,84 @@
 import React from 'react'
-import { Grid,Input, Typography } from '@mui/material'
+import { Grid, Input, Typography, Button } from '@mui/material'
 
-const userForm = (props) => {
+const UserForm = (props) => {
   return (
     <Grid
-        container
-        spacing={2}
-        sx={{ 
-          backgroundColor: '#ffffff',
-          marginBottom: '20px',
-          disply: 'block', 
+      container
+      spacing={2}
+      sx={{ 
+        backgroundColor: '#ffffff',
+        marginBottom: '20px',
+        display: 'block', 
+        padding: '20px',
+        borderRadius: '8px',
+      }}
+    >
+      <Grid item xs={12}>
+        <Typography component={'h1'} sx={{ color: '#000000'}}>User Form</Typography>
+      </Grid>
+  <Grid item xs={12}  sm={6} sx={{ display:'flex'}}>
+        <Typography
+          component={'label'}
+          htmlFor="Name"
+          sx={{
+            color: '#000000',
+            marginRight: '10px',
+            fontSize: '16px',
+            display: 'block',
+            fontWeight: 'bold'
           }}
+        >
+          Name
+        </Typography>
+        <Input
+          type="text"
+          id="Name"
+          name="Name"
+          sx={{ width: '10%' }}
+          value={''}
+          onChange={e =>{}}
+        />
+      </Grid>
 
-          >
-            <Grid item xs={12}>
-              <Typography component={'h1'} sx={{ color: '#000000'}}>User Form</Typography>
-            </Grid>
-
-            <Grid item xs={12} sm={6} sx={{ display:'flex'}}>
-              <Typography
-                component={'label'}
-                htmlFor="ID"
-                sx={{
-                  color: '#000000',
-                  marginRight: '10px',
-                  fontSize: '16px',
-                  display: 'block',
-                  fontWeight: 'bold'}}
-              >
-                
-                ID</Typography>
-
-                <Input
-                  type="number"
-                  id="ID"
-                  name="ID"
-                  sx={{ width: '100%' }}
-                  value={''}
-                  onChange={e =>{}}
-                />
-            </Grid>
-            <button sx={{
-              backgroundColor: '#4CAF50',
-              color: 'white',
-              padding: '10px 15px',
-              border: 'none',
-              borderRadius: '5px',
-              cursor: 'pointer',
-              '&:hover': {
-                backgroundColor: '#45a049',
-                
-              },
-            }}>
-              ADD
-            </button>
-          </Grid>
+      <Grid item xs={12}  sm={6} sx={{ display:'flex'}}>
+        <Typography
+          component={'label'}
+          htmlFor="ID"
+          sx={{
+            color: '#000000',
+            marginRight: '10px',
+            fontSize: '16px',
+            display: 'block',
+            fontWeight: 'bold'
+          }}
+        >
+          ID
+        </Typography>
+        <Input
+          type="number"
+          id="ID"
+          name="ID"
+          sx={{ width: '10%' }}
+          value={''}
+          onChange={e =>{}}
+        />
+      </Grid>
+      <Grid item xs={12} sx={{ display: 'block', marginTop: '20px' }}>
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: '#0d30dcff',
+            '&:hover': {
+              backgroundColor: '#0773d7ff',
+            },
+          }}
+        >
+          ADD
+        </Button>
+      </Grid>
+    </Grid>
   )
 }
 
-export default userForm
+export default UserForm
