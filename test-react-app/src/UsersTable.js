5a95dae36e5e3b0e30597f8de14e9e1268f956c6
Heshani833 <heshanishehana123@gmail.com>
2025-08-05 00:57:55 +0530

    Create table

--- test-react-app/src/UsersTable.js
+++ test-react-app/src/UsersTable.js
@@ -1,24 +1,38 @@
-import { Paper, Tab, Table, TableCell, TableContainer, TableHead } from '@mui/material'
-import React from 'react'
+import { Paper, Table, TableCell, TableContainer, TableHead, TableBody, TableRow } from '@mui/material';
+import React from 'react';
 
-const UsersTable = (props) => {
+const UsersTable = ({ rows }) => {
   return (
-<TableContainer component={Paper}>
-    <table>
+    <TableContainer component={Paper}>
+      <Table>
         <TableHead>
-            <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell>ID</TableCell>
-                <TableCell>Actions</TableCell>          //include edit and delete buttons
-            </TableRow>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>ID</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
         </TableHead>
         <TableBody>
-
+          {
+            rows.length > 0 ? rows.map(row => (
+              <TableRow key={row.ID} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell>{row.Name}</TableCell>
+                <TableCell>{row.ID}</TableCell>
+                <TableCell>
+                  <button style={{ marginRight: '10px' }} onClick={() => { }}>Update</button>
+                  <button onClick={() => { }}>Delete</button>
+                </TableCell>
+              </TableRow>
+            )) : (
+              <TableRow>
+                <TableCell colSpan={3}>No Data</TableCell>
+              </TableRow>
+            )
+          }
         </TableBody>
-    </table>
-</TableContainer>
+      </Table>
+    </TableContainer>
+  );
+};
 
-  )
-}
-
-export default UsersTable
+export default UsersTable;
--- test-react-app/src/users.js
+++ test-react-app/src/users.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import UserForm from './userForm'; // Make sure this file exists and default-exports a component
+import { Box } from '@mui/material';
+import UserForm from './UserForm'; // Make sure this file exists and default-exports a component
+import UsersTable from './UsersTable';
 
-const users = () => {
+
+const usersData =[
+  {
+ID: 1,
+Name: 'John Doe',
+  Actions: 'Edit/Delete' // Placeholder for actions, you can replace with actual buttons later
+  },
+  {
+ID: 2,
+Name: 'Jane Smith',
+  Actions: 'Edit/Delete'
+  },
+];
+
+
+const Users = () => {
   return (
-    <UserForm />
+    <Box 
+      sx={{width: 'calc(100% - 100px)', margin: 'auto',marginTop: '100px'}} >  {/* you also can use Grid or react fragmentations */}
+      <UserForm />
+      <UsersTable rows={usersData} />
+    </Box>
   );
 }
 
-export default users;
+export default Users;
